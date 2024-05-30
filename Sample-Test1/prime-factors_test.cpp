d950119a08991b395d43cdd763c125de0467eef3
min.byun <leaftotoro@gmail.com>
2024-05-30 11:51:05 +0900

    add of(3)

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -20,3 +20,8 @@ TEST_F(PrimeFixture, Of2) {
 	vector<int> expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
+TEST_F(PrimeFixture, Of3) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {3};
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
