3a1443ece2f462e3406bffdfaf1116c18dd617d3
min.byun <leaftotoro@gmail.com>
2024-05-30 11:44:11 +0900

    add of(1)

--- Day8_PrimeFactors/prime-factors.cpp
+++ Day8_PrimeFactors/prime-factors.cpp
@@ -1,3 +1,10 @@
-class PrimeFactor {
+#include <vector>
+using namespace std;
 
+class PrimeFactor {
+public:
+	vector<int> of(int i) {
+		vector<int> result = {};
+		return result;
+	}
 };
--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -3,9 +3,8 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFactors, PrimeTest) {
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
